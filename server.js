478a36195949e27b64f973279ae98589a9cbe546
Max Carlquist <tenkaklet@gmail.com>
2017-03-12 18:09:59 +0100

    initial and final commit

---
+++ server.js
@@ -0,0 +1,73 @@
+
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const {ShoppingList, Recipes} = require('./models');
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+// log the http layer
+app.use(morgan('common'));
+
+// we're going to add some items to ShoppingList
+// so there's some data to look at
+ShoppingList.create('beans', 2);
+ShoppingList.create('tomatoes', 3);
+ShoppingList.create('peppers', 4);
+
+// adding some recipes to `Recipes` so there's something
+// to retrieve.
+Recipes.create(
+  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
+Recipes.create(
+  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
+
+// when the root of this router is called with GET, return
+// all current ShoppingList items
+app.get('/shopping-list', (req, res) => {
+  res.json(ShoppingList.get());
+});
+
+app.post('/shopping-list', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'budget'];
+
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = ShoppingList.create(req.body.name, req.body.budget);
+  res.status(201).json(item);
+});
+
+app.post('/recipes', jsonParser, (req,res) => {
+
+  const [name, ingredients] = ['name', 'ingredients'];
+
+  if (!name in req.body || !ingredients in req.body) {
+    const message = 'No name or ingredients are found';
+    console.log(message);
+    return res.status(400).send(message);
+  } else {
+    const reItem = Recipes.create(req.body.name, req.body.ingredients);
+    res.status(201).json(reItem);
+  }
+});
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+})
+
+// app.post('/recipes')
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
